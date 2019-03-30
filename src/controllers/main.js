436bd7a9829dda74f18b386b12e8ae4077233480
Cristhian <csotelo88@hotmail.com>
2019-03-30 16:16:05 -0600

    navegation to fac/dept pages

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -34,18 +34,63 @@ router.post('/GetSchools', (req, res) => {
 });
 
 
+router.post('/GetFaculties', (req, res) => {
+  //dont have to do nothing lol
 
-/*
-router.post('/admin/logout', (req, res) => {
-    //dont have to do nothing lol
+  res.send(JSON.stringify({
+    items : [{
+      img : "https://www.nbn.org.il/wp-content/uploads/2014/01/engineering_mechanical_3042380_cropped.jpg",
+      id : 1,
+      name: "Engineering"
+      }, {
+        img :"https://d2ai0ibaxpbki1.cloudfront.net/v2/images/collections/video-music-licensing-collection-optimistic.jpg",
+        id : 2,
+        name: "Science"
+      },{
+        img :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwFhPeuIV3Yytxh8z-tkvYDsfYuQcaXG-hYaKR76bl715RBUkw",
+        id : 3,
+        name: "Fine Arts"
+      } ,{
+        img :"https://pbs.twimg.com/profile_images/787764476078587904/vcAZZNg1_400x400.jpg",
+        id : 4,
+        name: "Kinesiology"
+      },{
+        img :"https://www.dbs.ie/images/default-source/default-album/law-dbs.jpg?sfvrsn=1ee3bee9_1&MaxWidth=680&MaxHeight=&ScaleUp=false&Quality=High&Method=ResizeFitToAreaArguments&Signature=8213D2FDF018878A6EEE96858D640DA49E296056",
+        id : 5,
+        name: "Law"
+      }]
+  }));
 
-    res.send(JSON.stringify({
-        success :true
-    }));
+});
 
+router.post('/GetDepts', (req, res) => {
+  //dont have to do nothing lol
 
-}); */
+  res.send(JSON.stringify({
+    items : [{
+      img : "https://www.nbn.org.il/wp-content/uploads/2014/01/engineering_mechanical_3042380_cropped.jpg",
+      id : 1,
+      name: "Computer Science"
+      }, {
+        img :"https://d2ai0ibaxpbki1.cloudfront.net/v2/images/collections/video-music-licensing-collection-optimistic.jpg",
+        id : 2,
+        name: "Chemistry"
+      },{
+        img :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwFhPeuIV3Yytxh8z-tkvYDsfYuQcaXG-hYaKR76bl715RBUkw",
+        id : 3,
+        name: "Neuroscience"
+      } ,{
+        img :"https://pbs.twimg.com/profile_images/787764476078587904/vcAZZNg1_400x400.jpg",
+        id : 4,
+        name: "Biology"
+      },{
+        img :"https://www.dbs.ie/images/default-source/default-album/law-dbs.jpg?sfvrsn=1ee3bee9_1&MaxWidth=680&MaxHeight=&ScaleUp=false&Quality=High&Method=ResizeFitToAreaArguments&Signature=8213D2FDF018878A6EEE96858D640DA49E296056",
+        id : 5,
+        name: "Physics"
+      }]
+  }));
 
+});
 
 
 module.exports = router;
