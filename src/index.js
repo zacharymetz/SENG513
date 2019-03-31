308bfefb3b76b2d46729cb041f726353efe89ab4
Cristhian <csotelo88@hotmail.com>
2019-03-30 22:47:46 -0600

    added new dependencies
    cookie-parser, express-session, socket.io, server is http now

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 const express = require('express');
-const nunjucks = require('nunjucks');
-
-const bodyParser = require('body-parser');
-
 const app = express();
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 app.use('/static', express.static(__dirname + '/static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use(cookieParser());
 
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 });
+
+app.use(session({
+    secret: 'this_Is_A_Prerequistes_Secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000*60*60*24*14
+    }
+}));
 
 var adminGrid = require('./controllers/adminGrid');
 var admin = require('./controllers/admin');
@@ -24,4 +35,6 @@ app.use('/adminGrid', adminGrid);
 app.use('/', main);
 
 //listen at 3000
-app.listen(3000, () => console.log('server started'));
+http.listen(3000, function(){
+  console.log('Server started, listening on *:3000');
+});
