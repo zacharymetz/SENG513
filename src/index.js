5a478d65089662939e81048855d7f52ae4c17c80
Cristhian <csotelo88@hotmail.com>
2019-03-31 00:55:49 -0600

    removed cookie-parser and session
    Also got client's ip info, on client side code though.

--- src/index.js
+++ src/index.js
@@ -4,27 +4,15 @@ const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
 
 app.use('/static', express.static(__dirname + '/static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 });
-
-app.use(session({
-    secret: 'this_Is_A_Prerequistes_Secret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 1000*60*60*24*14
-    }
-}));
 
 var adminGrid = require('./controllers/adminGrid');
 var admin = require('./controllers/admin');
--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -116,4 +116,21 @@ function renderCourseGrid(requestData=null){
 $(()=>{
   renderSchoolGrid();
   gotoPage("home-page");
+  ipLookUp();
+  console.log(localStorage.length);
 });
+
+function ipLookUp () {
+  $.ajax('http://ip-api.com/json')
+  .then(
+      function success(response) {
+          console.log('User\'s Location Data is ', response);
+          console.log('User\'s Country', response.country);
+      },
+
+      function fail(data, status) {
+          console.log('Request failed.  Returned status of',
+                      status);
+      }
+  );
+}
