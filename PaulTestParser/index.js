3fe2bb21ec83a1c4ee72dfc2e0da29357dd8e84e
pauldan24 <32079319+pauldan24@users.noreply.github.com>
2019-04-07 18:23:19 -0600

    moving to dektop

--- PaulTestParser/index.js
+++ PaulTestParser/index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const fs = require('fs');
-
 const express = require('express');
 const multer = require('multer');
 const csv = require('fast-csv');
@@ -12,6 +11,69 @@ const app = express();
 const server = http.createServer(app);
 const port = 3000
 
+//for database
+const  { Pool,Client } = require('pg'); 
+const pool = new Client({
+  user: 'postgres',
+  host: '96.51.140.32',
+  database: 'webapp',
+  password: 'DUBTmNiaWMe9v6dqNnhiOQTrZEWmI1x34CfZOziWvHfWNpAKRBDIZ5KLmTCX0CZre7l03a7NeYM3LGuHLerl6hjgorTmFHW0',
+  port: 5432,
+});
+
+//connect to the pg admin instance
+pool.connect();
+db = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  }
+}
+
+
+
+
+
+checkFac("phfy",(data)=>{
+  console.log(data);  //returns all the rows that have AR in them 
+});
+
+/*based on a code that I passs in which will then be just extracted from the row of the 
+csv I need to check if the code exists then if it doesnt just create a new ID for that user. 
+*/
+function checkFac (code,next) {
+  var sqlQuery = "";
+  sqlQuery += "SELECT academicgroupid";
+  sqlQuery += "	FROM public.academicgroup WHERE code=$1;";
+  sqlParams = [code];
+
+  db.query(sqlQuery,sqlParams ,(err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if(result.rows.length > 0){
+        next(result.rows) //data is now result.rows
+      }else{
+        console.log("Doesn't exist");
+        sqlQuery = "";
+        sqlQuery += "INSERT INTO public.academicgroup";
+        sqlQuery += "(code, longname)";
+        sqlQuery += "VALUES ($1, $2) returning *;";
+        sqlParams = ['pp', 'PaulDan'];
+        db.query(sqlQuery,sqlParams ,(err1, result1) => {
+          if (err1) {
+            console.log(err1);
+          } else {
+            next(result1.rows)  //data will now be the result that doesn't exist
+          }
+        });
+      }
+    }
+  });
+
+}
+
+
+
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,13 +82,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 /*Routes*/
 app.get('/', (req,res) => {
   //render the index with all the corresponding messages in place.
-
   res.render('index');  //render the index html file
 });
 
 app.post('/', upload.single('files[]'), function (req, res) {
   const fileRows = [];
-
   // open uploaded file
   csv.fromPath(req.file.path)
     .on("data", function (data) {
@@ -38,7 +98,6 @@ app.post('/', upload.single('files[]'), function (req, res) {
       fs.unlinkSync(req.file.path);   // remove temp file
 
     })
-
     res.send(fileRows);
 });
 
@@ -52,7 +111,6 @@ function startServer() {
 setImmediate(startServer);
 
 function readCSVPassedIn(filePath) {
-
   // console.log(filePath);
   fs.createReadStream(filePath)
   .pipe(csv1())
@@ -64,6 +122,5 @@ function readCSVPassedIn(filePath) {
           console.log("This row is empty for this column");
       }
   });
-
-
 }
+
