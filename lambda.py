1d30d42e4eb781481d0fb4c815e7516b3b30e8bf
Zachary Metz <zachary.metz@ucalgary.ca>
2019-04-14 23:16:13 -0600

    lambda cide

---
+++ lambda.py
@@ -0,0 +1,87 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+
+
+    print(event['email'])
+    print(event['inquiry'])
+    print(event['firstName'])
+    print(event['lastName'])
+    print(event['type'])
+    print(event['template'])
+
+
+    # Replace sender@example.com with your "From" address.
+    # This address must be verified with Amazon SES.
+    SENDER = "tonybountea@gmail.com"
+    
+    # Replace recipient@example.com with a "To" address. If your account 
+    # is still in the sandbox, this address must be verified.
+    RECIPIENT = event['email']
+    
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the 
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    #CONFIGURATION_SET = "ConfigSet"
+    
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "us-west-2"
+    
+    # The subject line for the email.
+    SUBJECT = "Amazon SES Test (SDK for Python)"
+    
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("")
+                
+    # The HTML body of the email.
+    BODY_HTML = event['template']         
+    
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+    
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses',region_name=AWS_REGION)
+    
+    # Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            #ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+    
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
