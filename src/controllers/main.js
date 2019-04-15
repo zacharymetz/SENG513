a4968167084f50c220e04152c6adba0c2ba46bd1
Cristhian <csotelo88@hotmail.com>
2019-04-14 20:26:50 -0600

    some updates
    -color depts
    -top bar color

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -5,6 +5,7 @@ const db = require('../db');
 const express = require('express');
 var queryBuilder = require('./helpers/queryBuilder');
 var router = express.Router();
+var deptColors = ["#feffd1","#cfd5e2","#cefbf8","#c0a3c4","#d3ffce","#ffb90f","#87cefa","#eeaeee","#cdcd00","#fffafa"]
 
 router.get('/', (req, res) => {
   res.render('main/index.html');
@@ -16,7 +17,7 @@ router.post('/GetSchools', (req, res) => {
   if(city == null){
     city = "calgary";
   }
-  db.query("SELECT * FROM public.institution where cityid = (select cityid from geo.city where name = $1);",[city],(err,result)=>{
+  db.query("SELECT *, (select location from file where fileid=logoimage) as \"logo\",(select location from file where fileid=backgroundimage) FROM public.institution where cityid = (select cityid from geo.city where name = $1);",[city],(err,result)=>{
     if(err){
       //  if there is an error form the sql server with request
       res.send(JSON.stringify({
@@ -24,6 +25,7 @@ router.post('/GetSchools', (req, res) => {
         message : "db error"
       }));
     }else{
+      console.log(result.rows);
       var html = "";
       if (result.rows.length === 0) {
         html = "<div class=\"info\">Sorry, no schools found :(</div>"
@@ -32,8 +34,9 @@ router.post('/GetSchools', (req, res) => {
         for(var i=0;i<result.rows.length;i++){
           html +=  '<div class="collectItem">'
           html +=  '  <div>'
-          html +=  '    <img id="school-card-'+result.rows[i].institutionid+'" class="barLogo" style="cursor:pointer;" src="/static/img/UofC.png">'
-          html +=  '  <div id="school-name-'+result.rows[i].institutionid+'" style="display:none;">'+result.rows[i].name+'</div>'
+          html +=  '    <img id="school-card-'+result.rows[i].institutionid+'" class="barLogo" style="cursor:pointer;" src="'+result.rows[i].logo+'">'
+          html +=  '    <div id="school-name-'+result.rows[i].institutionid+'" style="display:none;">'+result.rows[i].name+'</div>'
+          html +=  '    <div id="school-color-'+result.rows[i].institutionid+'" style="display:none;">'+result.rows[i].brandcolor0+'</div>'
           html +=  '  </div>'
           html +=  '</div>'
         }
@@ -66,26 +69,57 @@ router.post('/GetFaculties', (req, res) => {
       }
       else {
         for(var i=0;i<result.rows.length;i++){
-          var facName = result.rows[i].longname.toLowerCase();
+          var facName = result.rows[i].facultycode;
           var facImg;
-          if (facName.includes("engineer")) facImg = "https://www.nbn.org.il/wp-content/uploads/2014/01/engineering_mechanical_3042380_cropped.jpg";
-          else if (facName.includes("science")) facImg = "https://d2ai0ibaxpbki1.cloudfront.net/v2/images/collections/video-music-licensing-collection-optimistic.jpg";
-          else if (facName.includes("fine")) facImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwFhPeuIV3Yytxh8z-tkvYDsfYuQcaXG-hYaKR76bl715RBUkw";
-          else if (facName.includes("kinesiology")) facImg = "https://pbs.twimg.com/profile_images/787764476078587904/vcAZZNg1_400x400.jpg";
-          else if (facName.includes("law")) facImg = "http://village.photos/images/user/9b3ad69f-d70d-4835-aeaa-9ee70e7bef36/24981801-a56b-4284-a4cd-21204c2d7422.jpg";
-          else if (facName.includes("business")) facImg = "https://images.freeimages.com/images/large-previews/adf/sun-burst-1478549.jpg";
-          else if (facName.includes("veterinary")) facImg = "https://blogs.ufv.ca/science/files/2016/10/WestGen-2016-Awards.png";
-          else if (facName.includes("medicine")) facImg = "https://www.intercleanshow.com/-/media/websites/interclean/isa/images/news/isa2018-healthcare-cleaning-1100x400.ashx";
-          else if (facName.includes("education")) facImg = "https://cdn20.patchcdn.com/users/22896301/20180813/011024/styles/T800x600/public/processed_images/school_4-1534180194-2580.jpg";
-          else if (facName.includes("art")) facImg = "https://sloanreview.mit.edu/content/uploads/2017/04/Gen-Michelman-End-of-Corporate-Culture-1200-1200x627.jpg";
-          else if (facName.includes("nursing")) facImg = "https://demedbook.com/images/2/how-do-clinical-trials-work-and-who-can-participate_7.jpg";
-          else if (facName.includes("environmental")) facImg = "https://ariahesaraki.files.wordpress.com/2014/01/skygardenhouse01.jpg";
-          else if (facName.includes("social")) facImg = "https://blogs.ufv.ca/swhs/files/2018/03/Hand-tree-purchased-image-from-Marcom-Copy.jpg";
+          if (facName.includes("EN")) {
+            facName = "Engineering";
+            facImg = "https://www.nbn.org.il/wp-content/uploads/2014/01/engineering_mechanical_3042380_cropped.jpg";
+          }
+          else if (facName.includes("SC")) {
+            facName = "Science";
+            facImg = "https://d2ai0ibaxpbki1.cloudfront.net/v2/images/collections/video-music-licensing-collection-optimistic.jpg";
+          }
+          else if (facName.includes("KN")) {
+            facName = "Kinesiology";
+            facImg = "https://pbs.twimg.com/profile_images/787764476078587904/vcAZZNg1_400x400.jpg";
+          }
+          else if (facName.includes("LA")) {
+            facName = "Law";
+            facImg = "http://village.photos/images/user/9b3ad69f-d70d-4835-aeaa-9ee70e7bef36/24981801-a56b-4284-a4cd-21204c2d7422.jpg";
+          }
+          else if (facName.includes("VM")) {
+            facName = "Veterinary Medicine";
+            facImg = "https://blogs.ufv.ca/science/files/2016/10/WestGen-2016-Awards.png";
+          }
+          else if (facName.includes("MD")) {
+            facName = "Medicine";
+            facImg = "https://www.intercleanshow.com/-/media/websites/interclean/isa/images/news/isa2018-healthcare-cleaning-1100x400.ashx";
+          }
+          else if (facName.includes("ED")) {
+            facName = "Education";
+            facImg = "https://cdn20.patchcdn.com/users/22896301/20180813/011024/styles/T800x600/public/processed_images/school_4-1534180194-2580.jpg";
+          }
+          else if (facName.includes("AR")) {
+            facName = "Arts";
+            facImg = "https://sloanreview.mit.edu/content/uploads/2017/04/Gen-Michelman-End-of-Corporate-Culture-1200-1200x627.jpg";
+          }
+          else if (facName.includes("NU")) {
+            facName = "Nursing";
+            facImg = "https://demedbook.com/images/2/how-do-clinical-trials-work-and-who-can-participate_7.jpg";
+          }
+          else if (facName.includes("EV")) {
+            facName = "Environmental Design";
+            facImg = "https://ariahesaraki.files.wordpress.com/2014/01/skygardenhouse01.jpg";
+          }
+          else if (facName.includes("SW")) {
+            facName = "Social Work";
+            facImg = "https://blogs.ufv.ca/swhs/files/2018/03/Hand-tree-purchased-image-from-Marcom-Copy.jpg";
+          }
           else facImg = "https://www.fincastleherald.com/wp-content/uploads/2017/08/school_icon_1501655097.png";
 
           html +=  '<div class="collectItem">'
           html +=  '  <div id="faculty-card-'+result.rows[i].facultyid+'" class="collectImage" style="background-image:url('+facImg+');"></div>'
-          html +=  '  <div id="faculty-card-'+result.rows[i].facultyid+'" class="collectName">'+result.rows[i].longname+'</div>'
+          html +=  '  <div id="faculty-card-'+result.rows[i].facultyid+'" class="collectName">'+facName+'</div>'
           html +=  '</div>'
         }
       }
@@ -118,11 +152,15 @@ router.post('/GetDepts', (req, res) => {
         html = "<div class=\"info\">Sorry, no departments found :(</div>"
       }
       else {
+        var colorIndex = 0;
         for(var i=0;i<result.rows.length;i++){
           html +=  '<div class="collectItem">'
-          html +=  '  <div id="dept-card-'+result.rows[i].departmentid+'"  class="collectImage" style="background-image:url('+"https://pbs.twimg.com/profile_images/787764476078587904/vcAZZNg1_400x400.jpg"+');"></div>'
-          html +=  '  <div id="dept-card-'+result.rows[i].departmentid+'" class="collectName">'+result.rows[i].name+'</div>'
+          html +=  '  <div id="dept-card-'+result.rows[i].departmentid+'"  class="collectImage" style="background-color:'+deptColors[colorIndex]+';">'
+          html +=  '    <div id="dept-card-'+result.rows[i].departmentid+'" class="deptName"><b>'+result.rows[i].code+'</b></div>'
+          html +=  '  </div>'
           html +=  '</div>'
+          colorIndex++;
+          if (colorIndex > (deptColors.length - 1)) colorIndex = 0;
         }
       }
       res.send(JSON.stringify({
